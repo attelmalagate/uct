9039e378009fed57e6ce6cea6c423749b9ea0c51
francois.court <francois.court@gmail.com>
2021-12-13 21:18:50 -0600

    v 1.2

--- uct-main.js
+++ uct-main.js
@@ -1,187 +1,85 @@
 /*
- * Universal Conversion Tool library, based on the Simple Web Framework
- *
- * version: 1.0
- * license: Apache 2.0
- * author:  François Court
- * date: october 2021
- *
- */
- 
-"use strict";
-export { 
-	cvGas,
-	getDescription,
-	version
-};
+* Universal Conversion Tool library, based on the Simple Web Framework
+*	uct-main.html script companion
+*
+* version: 1.2
+* license: Apache 2.0
+* author:  François Court
+* date: october 2021
+*
+*/
 import * as utils from './../swf/swf-utils.js';
-
-function version() {
-	return "uct main v1.0";
+import * as uct from './uct-lib.js';
+import * as tb from './../swf/swf-edtb.js';
+// instanciation of the generic swf ui object  
+const ui = new utils._ui({ nav: utils._ui.nav_opt.HIDDEN | utils._ui.nav_opt.HOME });
+function selectDefault() {
+	utils.triggerClickBySelector("[data-idtab='home']");
+	utils.triggerClickBySelector("[data-idsubtab='inabout-gui']");
 }
-
-class cvp {
-	constructor(cv, format, txt, min="") {
-		this.cv = cv;
-		this.format = format;
-		this.txt=txt;
-		this.min=min;
-	}
-};
-
-var flow_cvdatas = new Map([
-	['f_gs_scfyear', new cvp(1/38.040, '#,##0.000', 'scf/year (millions)')], 
-	['f_gs_scfmonth',new cvp(12/38.040, '#,##0.000', 'scf/month (millions)')] ,
-	['f_gs_scfday',  new cvp(365.25/1000/38.040, '#,##0.000', 'scf/day (thousands)')],
-	['f_gs_scfhour', new cvp(365.25*24/1000000/38.040, '#,##0.', 'scf/hour')],
-	['f_gs_nm3year', new cvp(1, '#,##0.000', 'm3/year (millions)')], 
-	['f_gs_nm3month',new cvp(12, '#,##0.000', 'm3/month (millions)')],
-	['f_gs_nm3day',  new cvp(365.25/1000000, '#,##0.', 'm3/day')],
-	['f_gs_nm3hour', new cvp(365.25*24/1000000, '#,##0.', 'm3/hour')],
-	['f_gs_sm3year', new cvp(1*273.15/288.15, '#,##0.000', 'm3/year (millions)')], 
-	['f_gs_sm3month',new cvp(12*273.15/288.15, '#,##0.000', 'm3/month (millions)')],
-	['f_gs_sm3day',  new cvp(365.25/1000000*273.15/288.15, '#,##0.', 'm3/day')],
-	['f_gs_sm3hour', new cvp(365.25*24/1000000*273.15/288.15, '#,##0.', 'm3/hour')],
-	['f_o2_stpd',    new cvp(365.25*24/1000000/(1.4287*24/1000)*0.90718474, '#,##0.000', 'short tons/day')], 
-	['f_o2_gapd',    new cvp(365.25*24/1000000/(1.4287*24)*1141.2/1000*3.785411784, '#,##0.0', 'gallons/day')],
-	['f_o2_mtpd',    new cvp(365.25*24/1000000/(1.4287*24/1000), '#,##0.000', 'metric tons/day')],
-	['f_o2_lipd',    new cvp(365.25*24/1000000/(1.4287*24)*1141.2/1000, '#,##0.', 'liters/day')],
-	['f_n2_stpd',    new cvp(365.25*24/1000000/(1.2501*24/1000)*0.90718474, '#,##0.000', 'short tons/day')], 
-	['f_n2_gapd',    new cvp(365.25*24/1000000/(1.2501*24)*806.11/1000*3.785411784, '#,##0.0', 'gallons/day')],
-	['f_n2_mtpd',    new cvp(365.25*24/1000000/(1.2501*24/1000), '#,##0.000', 'metric tons/day')],
-	['f_n2_lipd',    new cvp(365.25*24/1000000/(1.2501*24)*806.11/1000, '#,##0.', 'liters/day')],
-	['f_ar_stpd',    new cvp(365.25*24/1000000/(1.7835*24/1000)*0.90718474, '#,##0.000', 'short tons/day')], 
-	['f_ar_gapd',    new cvp(365.25*24/1000000/(1.7835*24)*1395.4/1000*3.785411784, '#,##0.0', 'gallons/day')],
-	['f_ar_mtpd',    new cvp(365.25*24/1000000/(1.7835*24/1000), '#,##0.000', 'metric tons/day')],
-	['f_ar_lipd',    new cvp(365.25*24/1000000/(1.7835*24)*1395.4/1000, '#,##0.', 'liters/day')],
-	['f_co2_stpd',   new cvp(365.25*24/1000000/(1.9763*24/1000)*0.90718474, '#,##0.000', 'short tons/day')], 
-	['f_co2_gapd',   new cvp(365.25*24/1000000/(1.9763*24)*1178.4/1000*3.785411784, '#,##0.0', 'gallons/day')],
-	['f_co2_mtpd',   new cvp(365.25*24/1000000/(1.9763*24/1000), '#,##0.000', 'metric tons/day')],
-	['f_co2_lipd',   new cvp(365.25*24/1000000/(1.9763*24)*1178.4/1000, '#,##0.', 'liters/day')],
-	['f_h2_stpd',    new cvp(365.25*24/1000000/(0.08988*24/1000)*0.90718474, '#,##0.000', 'short tons/day')], 
-	['f_h2_gapd',    new cvp(365.25*24/1000000/(0.08988*24)*70.516/1000*3.785411784, '#,##0.0', 'gallons/day')],
-	['f_h2_mtpd',    new cvp(365.25*24/1000000/(0.08988*24/1000), '#,##0.000', 'metric tons/day')],
-	['f_h2_lipd',    new cvp(365.25*24/1000000/(0.08988*24)*70.516/1000, '#,##0.', 'liters/day')],
-	['f_he_lbpd',    new cvp(365.25*24/1000000/(0.1784*24/1000)*0.90718474/2000, '#,##0.0', 'pounds/day')], 
-	['f_he_gapd',    new cvp(365.25*24/1000000/(0.1784*24)*124.74/1000*3.785411784, '#,##0.0', 'gallons/day')],
-	['f_he_kgpd',    new cvp(365.25*24/1000000/(0.1784*24/1000)/1000, '#,##0.0', 'kilograms/day')],
-	['f_he_lipd',    new cvp(365.25*24/1000000/(0.1784*24)*124.74/1000, '#,##0.', 'liters/day')],
-	['f_he_isoyear', new cvp(1/38.040*0.954941, '#,##0.0', 'iso/year')],
-	['f_he_knm3year',new cvp(1/1000, '#,##0.000', 'm3 (thousands)/year')],
-	['f_he_ksm3year',new cvp(1*273.15/288.15/1000, '#,##0.000', 'm3 (thousands)/year')]
-]);
-
-var price_cvdatas = new Map([
-	['p_gs_hscf',  new cvp(1*38.040/100, "#,##0.000", "$/hscf")],
-	['p_gs_kscf',  new cvp(1*38.040/1000, "#,##0.000", "$/kscf")],
-	['p_gs_nm3',   new cvp(1, "#,##0.000", "$/Nm3")],
-	['p_gs_sm3',   new cvp(1/(273.15/288.15), "#,##0.000", "$/Sm3")],
-	['p_o2_st',    new cvp(1*(1.4287/1000)/0.90718474, "#,##0.00", "$/ston")],
-	['p_o2_mt',    new cvp(1*(1.4287/1000), "#,##0.00", "$/mton")],
-	['p_n2_st',    new cvp(1*(1.2501/1000)/0.90718474, "#,##0.00", "$/ston")],
-	['p_n2_mt',    new cvp(1*(1.2501/1000), "#,##0.00", "$/mton")],
-	['p_ar_st',    new cvp(1*(1.7835/1000)/0.90718474, "#,##0.00", "$/ston")],
-	['p_ar_mt',    new cvp(1*(1.7835/1000), "#,##0.00", "$/mton")],
-	['p_co2_st',   new cvp(1*(1.9763/1000)/0.90718474, "#,##0.00", "$/ston")],
-	['p_co2_mt',   new cvp(1*(1.9763/1000), "#,##0.00", "$/mton")],
-	['p_co2_gal',  new cvp(1*(1.9763/1000)/1178.4*1000000/3.785411784, "#,##0.00", "$/gallon")],
-	['p_co2_li',   new cvp(1*(1.9763/1000)/1178.4*1000000, "#,##0.00", "$/liter")],
-	['p_h2_lb',    new cvp(1*(0.08988/1000)*1000*2.204623, "#,##0.00", "$/lb")],
-	['p_h2_kg',    new cvp(1*(0.08988/1000)*1000, "#,##0.00", "$/kg")],
-	['p_he_li',    new cvp(1*(0.1784/1000)/124.74*1000000, "#,##0.00", "$/liter")],
-	['p_he_kg',    new cvp(1*(0.1784/1000)*1000, "#,##0.00", "$/kg")]
-]);
-
-var temp_cvdatas = new Map([
-	['t_fahr',    new cvp(1*5/9, "#,##0.0", "&deg;F", -273.15*9/5+32)],
-	['t_celsius', new cvp(1, "#,##0.0", "&deg;C",-273.15)],
-	['t_kelvin',  new cvp(1, "#,##0.00", "&deg;K",0)]
-]);
-// special case for temperatures - defines an offset to add depending on the conversion 
-var temp_temp = new Map([
-	['t_fahr',    new Map([['t_fahr',0],             ['t_celsius',32],     ['t_kelvin',-273.15*9/5+32]])],
-	['t_celsius', new Map([['t_fahr',-32*5/9],       ['t_celsius',0],      ['t_kelvin',-273.15]])],
-	['t_kelvin',  new Map([['t_fahr',+273.15-32*5/9],['t_celsius',273.15], ['t_kelvin',0]])]
-]);
-
-var pressure_cvdatas = new Map([
-	['pr_psi',  new cvp(1/14.50377, "#,##0.00", "psi")],
-	['pr_bar',  new cvp(1, "#,##0.00", "bar")],
-	['pr_mbar', new cvp(1/1000, "#,##0.0", "mbar")],
-	['pr_inhg', new cvp(1/29.529983, "#,##0.00", "in Hg")],
-	['pr_atm',  new cvp(1/0.9869233, "#,##0.00", "atm")]
-]);
-const all_cvdatas = new Map([
-	['f_', flow_cvdatas],
-	['p_', price_cvdatas],
-	['t_', temp_cvdatas],
-	['pr', pressure_cvdatas]
-]);
-
-// update all values in the same table related to the base element elt passed as parameter (a td element) 
-// td are related when they belong to the same category of conversion (first two letters of the id
-// must be the same)
-function cvGas(elt, value=null) {
-	// type of conversion pr=pressure, f_ flow etc.
-	var type=elt.id.substr(0,2);
-	// conversion map corresponding to the type of conversion
-	var cvdatas=all_cvdatas.get(type);
-	var rv=false;
-	// value of the base element
-	if (value != null) {
-		elt.textContent=value;
-	}
-	var cur_val=parseFloat(elt.textContent);
-	// get all nodes to update, ie whose id starts with the same two letters than the based element 
-	var nodes=elt.parentNode.parentNode.parentNode.querySelectorAll("[id^="+type+"]");
-	if (cvdatas !== undefined && !isNaN(cur_val)) {
-		// tre=true if temperature
-		var tre=(type === 't_');
-		// check if there is a minimu value associated with the base element
-		var cvdata=cvdatas.get(elt.id);
-		if (cvdata !== undefined) {
-				if (cvdata.min !== "") {
-					if (cur_val < cvdata.min) {
-						cur_val=cvdata.min;
-					}
-				}
-				// cval will be the base conversion factor for the other related elements
-				var cval=cur_val*cvdata.cv;
-				cur_val=utils.format(cvdata.format, cur_val);
-				// iterate the related elements of the table and update them from the new value of the base element
-				nodes.forEach( function(elt_target){
-					cvdata=cvdatas.get(elt_target.id);
-					if (cvdata !== undefined) {
-						var targetval=cval/cvdata.cv;
-						if (tre) {
-							// if we convert Tre, there is an additional offset to add, defined in the temp_temp matrix
-							var tt=temp_temp.get(elt_target.id);
-							var ttoffset=tt.get(elt.id);
-							targetval+=ttoffset;
-						}
-						elt_target.textContent=utils.format(cvdata.format, targetval);
-					}
-					else {
-						console.log("could not find the entry corresponding to " + elt_target.id);
-						console.log(elt_target);
-					}
-				});
-				elt.textContent=cur_val;
-				rv=true;
-			}
-		else {
-			console.log("could not find the entry corresponding to " + elt.id);
-			console.log(elt);
+function updateDOMElements() {
+	// add the same 'gases' row everytime they're needed (ie in an ".gas" class element)
+	utils.eltBySelAppend('.oxygen', '<td colspan="5" class="td2" style="background:LightSalmon">Oxygen</td>');
+	utils.eltBySelAppend('.nitrogen', '<td colspan="5" class="td2" style="color: white; background:DarkRed">Nitrogen</td>');
+	utils.eltBySelAppend('.argon', '<td colspan="5" class="td2" style="background:Teal">Argon</td>');
+	utils.eltBySelAppend('.co2', '<td colspan="5" class="td2" style="background:Gray">CO2</td>');
+	utils.eltBySelAppend('.hydrogen', '<td colspan="5" class="td2" style="color: white; background:#272727">Hydrogen</td>');
+	utils.eltBySelAppend('.helium', '<td colspan="5" class="td2" style="color: white;background:SaddleBrown">Helium</td>');
+	// reset all values to 0
+	new utils._nodes('.tdval').text('0').attr('contenteditable', 'true');
+	// update the label from the xxx_cvdatas map
+	new utils._nodes('.tdtxt').each(function (elt) {
+		const attr = elt.getAttribute('data-id');
+		if (attr !== null) {
+			elt.innerHTML = uct.getDescription(attr);
+		}
+	});
+	// some non null value by default to look better
+	uct.cvGas("f_gs_nm3hour", null, "100");
+	uct.cvGas("p_gs_nm3", null, "0.9");
+	uct.cvGas("t_celsius", null, "20");
+	uct.cvGas("pr_atm", null, "1 ");
+}
+updateDOMElements();
+document.addEventListener("DOMContentLoaded", function () {
+	// management of editable tables; everything is managed in the class
+	// including the invocation of the listener to click and keydown events
+	function onUpdate(elt) {
+		console.log(elt.textContent);
+		if (uct.cvGas(null, elt, elt.textContent) == false) {
+			elt.textContent = "conversion error";
 		}
 	}
-	return rv;
-}
-
-function getDescription (idc) {
-	var cvdatas=all_cvdatas.get(idc.substr(0,2));
-	var cvdata=cvdatas.get(idc);
-	if (cvdata !== undefined) {
-		return cvdata.txt;
-   }
-	return "";
-}
+	var tbed = new tb._edtb(onUpdate, { toutMax: 4000 });
+	//manage HTML imports
+	var tokens = [{
+			token: "{{name}}",
+			value: "Fran&#xE7;ois Court"
+		}, {
+			token: "{{email}}",
+			value: "attel@malagate.com"
+		}, {
+			token: "{{year}}",
+			value: "2021"
+		}];
+	utils.importsHTML(tokens);
+	window.addEventListener('resize', function () {
+		ui.resize();
+	});
+	// create the generic events handlers for the swf framework
+	ui.addEvents();
+	// validation of the default selected panels
+	selectDefault();
+	// check & manager the url query parameters 
+	ui.checkQueryParameters();
+	// hide nav panel if option HIDDEN is selected
+	if (ui.navOptHidden()) {
+		ui.hideNav();
+	}
+	ui.resize();
+	// hide the blocking div now that the page is loaded
+	ui.show();
+	console.log(utils.version());
+	console.log(uct.version());
+	console.log(tb.version());
+});
